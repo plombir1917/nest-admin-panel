cb4f9d4cd01c851a59de3b084d110a190cef9eae
plombir1917 <dfhhggh@list.ru>
2023-08-12 00:26:54 +0400

    jwtsecret replaced in .env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,14 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from 'src/account/entities/account.entity';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from '../constants/constants';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+config();
+
+const configService = new ConfigService();
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Account]),
     JwtModule.register({
       global: true,
-      secret: jwtConstants.secret,
+      secret: configService.get('JWTCONSTANT'),
       signOptions: { expiresIn: '60s' },
     }),
   ],
